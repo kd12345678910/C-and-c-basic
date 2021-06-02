d4f5e54fd03d74806cbd822b92cabffc2c513041
kishorgdeshmukh <61237085+kishorgdeshmukh@users.noreply.github.com>
2021-06-02 20:26:38 +0530

    Add files via upload
    Array

---
+++ array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    // array declaration
+    int i;
+    int arr[5]={10,20,30,40,50};
+    printf("Array elements are:\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+
+}
